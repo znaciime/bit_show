6ec7cd752e0980c8a3f5103f5bdfc630f2bd36be
BIT Student <classroom@bgit.rs>
2019-03-21 19:29:14 +0100

    new

--- index2js.js
+++ index2js.js
@@ -2,25 +2,30 @@ const index2 = (() => {
 
     const idShow = localStorage.getItem("showname");
     console.log(idShow);
-    const index2search = `http://api.tvmaze.com/shows/${idShow}?embed[]=episodes&embed[]=cast`;
+    const index2search = `http://api.tvmaze.com/shows/${idShow}?embed[]=seasons&embed[]=cast`;
 
-    $.get(`${index2search}`, function (data2) {
-        console.log(data2);
+    $.get(`${index2search}`, function (singleShowData) {
+        console.log(singleShowData);
+        const castNames = [];
+        const listactors = $(".castnames");
 
+        singleShowData._embedded.cast.forEach(Element => {
+            const listNames = $("<li>").text(Element.person.name)
+            castNames.push(Element.person.name);
+            listactors.append(listNames);
+
+        });
 
         const foot = $(".foot");
-        const cast2 = data2.summary;
-        const titles = $("<h1>").text(data2.name);
-        const img2 = $("<img src='' >").attr("src", data2.image.medium);
+        const cast = $(".cast");
+
+        const summar = singleShowData.summary;
+        const titles = $("<h1>").text(singleShowData.name);
+        const img2 = $("<img src='' >").attr("src", singleShowData.image.medium);
         const blok2 = $(".index2div");
         blok2.append(titles);
         blok2.append(img2);
-        foot.append(cast2);
-
-
-
-
+        foot.append(summar);
     })
-
 
 })()
